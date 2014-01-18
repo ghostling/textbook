26544373e88518b0304e7db7510f0607ff602432
Jiexi Cao <jiexicao@gmail.com>
2014-01-18 01:35:37 -0500

    set up more skeleton code

---
+++ models.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import ndb
+
+class book(ndb.Model):
+    title = ndb.StringProperty(required=True)
+    authors = ndb.StringProperty(repeated=True)
+    isbn = ndb.StringProperty(required=True)
+    image = ndb.StringProperty() # just be a link to an image for now?
+    orig_price = ndb.FloatProperty(required=True)
+
