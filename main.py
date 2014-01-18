0b06d9177ffb4848cefc941e89c82059193c82a8
Jiexi Cao <jiexicao@gmail.com>
2014-01-18 01:36:00 -0500

    set up more skeleton code

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
+import models as md
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+def render_str(template, **params):
+    t = jinja_environment.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('Hello world!')
+        name = 'Bob'
+        self.render('index.html', name=name)
+
+    def post(self):
+        pass
+
+class BookHandler(BaseHandler):
+    def get(self, book_id):
+        self.write('Look up the book by id in DB and display stuff here')
+
+class CourseHandler(BaseHandler):
+    def get(self, course_id):
+        self.write('Look up the course by id in DB and display stuff here')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    (r'/book/(\d+)', BookHandler),
+    (r'/course/(\d+)', CourseHandler)
 ], debug=True)
